166e595bc7b900e3745e7b29321c1b1983de0d4c
Neil <neil.stoker@clixifix.co.uk>
2021-10-22 13:28:43 +0100

    fix: correct name of test function

--- hello_test.go
+++ hello_test.go
@@ -2,9 +2,9 @@ package main
 
 import "testing"
 
-func TextHello(t *testing.T) {
 	got := Hello("Neil")
 	want := "Hello, Neil!"
+func TestHello(t *testing.T) {
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
