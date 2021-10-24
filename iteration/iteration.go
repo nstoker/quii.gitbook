99e214b04120a3880b917f9553585b382913f412
Neil Stoker <neil.stoker@gmail.com>
2021-10-24 14:31:08 +0100

    task: Simple iteration

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,6 +1,11 @@
 package iteration
 
-func Repeat(character string) string {
+import "log"
+
+func Repeat(character string, iterations int) string {
+	if iterations < 0 {
+		log.Fatalf("invalid iteration count passed (%d)", iterations)
+	}
 	var repeated string
 	for i := 0; i < 5; i++ {
 		repeated = repeated + character
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,12 +1,22 @@
 package iteration
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	times := 5
+	repeated := Repeat("a", times)
+	expected := strings.Repeat("a", times)
 
 	if repeated != expected {
 		t.Errorf("got %q, want %q", repeated, expected)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 10)
 	}
 }
